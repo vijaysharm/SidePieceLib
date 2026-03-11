0a95b7646cece2688117bc2561c0d72d9f3aa4b6
vijaysharm <vijays@scribd.com>
2026-03-10 22:54:35 -0400

    Bump TCA version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.23.2"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.24.1"),
         .package(url: "https://github.com/vijaysharm/textual", branch: "main"),
     ],
     targets: [
